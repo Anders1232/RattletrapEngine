08769aa057a152a862dee6ccafa6f2574e058622
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-11-10 01:12:10 -0200

    Ajustado alguns whitespaces

--- include/Button.h
+++ include/Button.h
@@ -6,45 +6,45 @@
 #include "GameObject.h"
 
 class Button : public Component {
-  public:
-	enum State : int {
-		DISABLED = 0,
-		ENABLED,
-		HIGHLIGHTED,
-		PRESSED
-	};
-	typedef void (*ButtonCallbackFunc) (void*);
-	struct Callback {
-		Callback( ButtonCallbackFunc callbackFunc = nullptr, void* caller = nullptr );
-		ButtonCallbackFunc callbackFunc;
-		void* caller;
-		void Call();
-	};
-
-	Button(GameObject& associated, Button::State initialState = Button::State::ENABLED, bool interactOnBoundingBox = true);
-	~Button();
+	public:
+		enum State : int {
+			DISABLED = 0,
+			ENABLED,
+			HIGHLIGHTED,
+			PRESSED
+		};
+		typedef void (*ButtonCallbackFunc) (void*);
+		struct Callback {
+			Callback( ButtonCallbackFunc callbackFunc = nullptr, void* caller = nullptr );
+			ButtonCallbackFunc callbackFunc;
+			void* caller;
+			void Call();
+		};
 	
-	void EarlyUpdate( float dt );
-	void Update( float dt );
-	void LateUpdate( float dt );
-	void Render() const;
-	bool Is( ComponentType type ) const;
-
-	void SetCallback(Button::State stateToSet, Button::Callback calldata);
-	void SetReleaseCallback(Button::Callback calldata);
-	void SetState(Button::State newState);
-	Button::State GetState(void) const;
-	void Click();
-
-	bool interactOnBoundingBox;
-
-  private:
-	Button::State actualState;
-	Callback disabled;
-	Callback enabled;
-	Callback highlighted;
-	Callback pressed;
-	Callback released;
+		Button(GameObject& associated, Button::State initialState = Button::State::ENABLED, bool interactOnBoundingBox = true);
+		~Button();
+		
+		void EarlyUpdate( float dt );
+		void Update( float dt );
+		void LateUpdate( float dt );
+		void Render() const;
+		bool Is( ComponentType type ) const;
+	
+		void SetCallback(Button::State stateToSet, Button::Callback calldata);
+		void SetReleaseCallback(Button::Callback calldata);
+		void SetState(Button::State newState);
+		Button::State GetState(void) const;
+		void Click();
+	
+		bool interactOnBoundingBox;
+	
+	private:
+		Button::State actualState;
+		Callback disabled;
+		Callback enabled;
+		Callback highlighted;
+		Callback pressed;
+		Callback released;
 };
 
 #endif // BUTTON_H
--- include/Text.h
+++ include/Text.h
@@ -15,77 +15,77 @@
 #include "Vec2.h"
 
 class Text : public Component {
-  public:
-	enum TextStyle {
-		SOLID,/**< O texto é renderizado diretamente, sem nenhum tipo de tratamento visual.*/
-		SHARED,/**< o texto é renderizado em um retângulo da cor dada em um argumento Color no hora de renderizar.*/
-		BLENDED/**< Cria um texto com as bordas suavizadas (usando o canal alpha), para que ele se adapte melhor à região da tela onde for renderizado. É mais custoso de se renderizar que os outros dois, mas faz
-	uma diferença perceptível.*/
-	};
-
-	Text(GameObject associated);
-	~Text();
-
-	void EarlyUpdate( float dt );
-	void Update( float dt );
-	void LateUpdate( float dt );
-	bool Is( ComponentType type ) const;
-	void Render() const;
-
-	/**
-		\brief Altera o texto.
-
-		Altera a posição do texto.
-	*/
-	void SetText(std::string text);
-	/**
-		\brief Altera a cor.
-
-		Altera a cor do texto.
-	*/
-	void SetColor(Color color);
-	/**
-		\brief Altera a cor de fundo.
-
-		Altera a cor de fundo do texto. Válido somente para quando se usa TextStyle::SHARED.
-	*/
-	void SetBgColor(Color color);
-	/**
-		\brief Altera o estilo.
-
-		Altera o estilo do texto.
-	*/
-	void SetStyle(TextStyle style);
-	/**
-		\brief Altera o tamanho da fonte.
-
-		Altera o estilo do texto.
-	*/
-	void SetFontSize(int fontSize);
-	/**
-		\brief Altera a fonte do test.
-
-		Altera a fonte do texto.
-	*/
-	void SetFont( std::string fontFile );
-	/**
-		\brief Obtém o tamanho do texto.
-
-		Obtém o tamanho do texto em pixels.
-	*/
-	Vec2 GetSize(void) const;
-
-  private:
-	void RemakeTexture(void);
-	std::shared_ptr<TTF_Font> font;
-	SDL_Texture* texture;
-	std::string text;
-	TextStyle style;
-	int fontSize;
-	Color color;
-	Color bgColor;
-	std::string fontFile;
-	Vec2 fontDimensions;
+	public:
+		enum TextStyle {
+			SOLID,/**< O texto é renderizado diretamente, sem nenhum tipo de tratamento visual.*/
+			SHARED,/**< o texto é renderizado em um retângulo da cor dada em um argumento Color no hora de renderizar.*/
+			BLENDED/**< Cria um texto com as bordas suavizadas (usando o canal alpha), para que ele se adapte melhor à região da tela onde for renderizado. É mais custoso de se renderizar que os outros dois, mas faz
+		uma diferença perceptível.*/
+		};
+	
+		Text(GameObject associated);
+		~Text();
+	
+		void EarlyUpdate( float dt );
+		void Update( float dt );
+		void LateUpdate( float dt );
+		bool Is( ComponentType type ) const;
+		void Render() const;
+	
+		/**
+			\brief Altera o texto.
+	
+			Altera a posição do texto.
+		*/
+		void SetText(std::string text);
+		/**
+			\brief Altera a cor.
+	
+			Altera a cor do texto.
+		*/
+		void SetColor(Color color);
+		/**
+			\brief Altera a cor de fundo.
+	
+			Altera a cor de fundo do texto. Válido somente para quando se usa TextStyle::SHARED.
+		*/
+		void SetBgColor(Color color);
+		/**
+			\brief Altera o estilo.
+	
+			Altera o estilo do texto.
+		*/
+		void SetStyle(TextStyle style);
+		/**
+			\brief Altera o tamanho da fonte.
+	
+			Altera o estilo do texto.
+		*/
+		void SetFontSize(int fontSize);
+		/**
+			\brief Altera a fonte do test.
+	
+			Altera a fonte do texto.
+		*/
+		void SetFont( std::string fontFile );
+		/**
+			\brief Obtém o tamanho do texto.
+	
+			Obtém o tamanho do texto em pixels.
+		*/
+		Vec2 GetSize(void) const;
+	
+	private:
+		void RemakeTexture(void);
+		std::shared_ptr<TTF_Font> font;
+		SDL_Texture* texture;
+		std::string text;
+		TextStyle style;
+		int fontSize;
+		Color color;
+		Color bgColor;
+		std::string fontFile;
+		Vec2 fontDimensions;
 };
 
 #endif // TEXT_H
