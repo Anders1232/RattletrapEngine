a4730ccb524f3f2552648ce85c7be8ecae103854
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-12-12 12:48:21 -0200

    Corrigido erra na interface do bugfix no qual o Finder não era uma referência

--- include/NearestFinder.h
+++ include/NearestFinder.h
@@ -15,8 +15,8 @@ class Finder
 template<class T>
 class NearestFinder{
 	public:
-		virtual T* FindNearest(Vec2 origin, Finder<T> finder, float range= std::numeric_limits<float>::max()) const = 0;
-		virtual std::vector<T*>* FindNearests(Vec2 origin, Finder<T> finder, float range= std::numeric_limits<float>::max()) const = 0 ;
+		virtual T* FindNearest(Vec2 origin, Finder<T> &finder, float range= std::numeric_limits<float>::max()) const = 0;
+		virtual std::vector<T*>* FindNearests(Vec2 origin, Finder<T> &finder, float range= std::numeric_limits<float>::max()) const = 0 ;
 		//lembrando que é responsabilidade do chamador a desalocação
 		virtual ~NearestFinder(){};
 };
