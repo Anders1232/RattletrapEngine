0571df9c1e0ef46151838a723ba889bfc87c44e3
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-12-30 20:20:41 -0200

    Iniciando implementação

---
+++ include/Thread.h
@@ -0,0 +1,24 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#define ALLOW_SDL_THREAD
+#define ALLOW_PTHREAD
+
+#
+
+namespace Rattletrap {
+	namespace Multithreading {
+		enum TheadLib{
+			SDL,
+			POSIX
+		};
+		class Thread{
+			public:
+				Thread()
+			private:
+		}
+	}
+	
+}
+
+#endif // THREAD_H
